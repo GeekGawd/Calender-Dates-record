1e58d5a03b08581e9fadf6cca08f0e22a826deb2
AlStien <aj601604@gmail.com>
2022-03-04 01:17:44 +0530

    upadated allowed hosts for hosting

--- addiction_trial/settings.py
+++ addiction_trial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6fc!9r$s+5fy_dyau1a(5ragu*87-t@5e@fq521etq#jmv7euj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
